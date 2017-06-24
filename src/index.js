3a2dafead891b3c994fb3388eaf7afb072bc09c3
joextodd <joextodd@gmail.com>
2017-06-24 15:59:24 +0100

    Fixed controls bugs

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import { Player } from './player'
 import './index.scss'
 
 const url = 'https://youtube.joextodd.com'
+const Use = (href, style) =>
+  h('svg', { class: style }, [
+    h('use', { oncreate: e =>
+    e.setAttributeNS(
+      'http://www.w3.org/1999/xlink',
+      'href',
+      href
+    )})
+  ])
 
 const homePage = (s,a) =>
-  h('section', {}, '', [
-    h('form', {}, '', [
+  h('section', {}, [
+    h('form', {}, [
       h('input', {
         type: 'text',
         placeholder: 'Enter YouTube URL...',
@@ -18,32 +27,30 @@ const homePage = (s,a) =>
 const lostPage = (s,a) =>
   h('h1', { onclick: e => a.router.go('/') }, 'Back to {location.hostname}')
 
-const controls = (id) =>
-  h('svg', { class: 'icon' }, '', [
-    h('use', { 'xlink:href': id })
-  ])
-
-const playPage = (s, a) =>
-  h('page', {}, '', [
-    h('main', {}, '', [
-      h('img', { src: `https://img.youtube.com/vi/${s.id}/hqdefault.jpg` }),
-      h('controls-', {}, '', [
-        h('button', {}, '', [ controls('#previous') ]),
-        h('button', { onclick: e => a.backwards() }, 'Back'),
-        h('button', { onclick: e => a.playPause() }, 'Play'),
-        h('button', { onclick: e => a.forwards() }, 'Forw'),
-        h('button', { onclick: e => a.nextVideo() }, 'Next'),
-      ]),
+const playPage = (s,a) =>
+  h('page', {}, [
+    h('main', {}, [
+      h('img', { src: s.id && `https://img.youtube.com/vi/${s.id}/hqdefault.jpg` }),
       h('title-', {}, s.track ? s.track.title : ''),
       h('audio', {
-        src: s.track ? `${url}/proxy/${s.track.url}` : '',
-        crossorigin: 'anonymous', //autoplay: 'yes',
+        src: s.track && `${url}/proxy/${s.track.url}`,
+        crossorigin: 'anonymous', autoplay: 'yes',
         oncreate: e => {
           e.onended = a.nextVideo
           e.ontimeupdate = s.iOS && a.iosDuration() && a.nextVideo()
+          s.player = e
         }
-      }, '')
-    ])
+      })
+    ]),
+    h('controls-', {}, [
+      h('button', {}, Use('#previous')),
+      h('button', { onclick: e => a.rewind() }, Use('#rewind')),
+      h('button', { onclick: e => a.playPause() },
+        s.playing ? Use('#pause', 'large') : Use('#play', 'large')
+      ),
+      h('button', { onclick: e => a.forwards() }, Use('#forwards')),
+      h('button', { onclick: e => a.nextVideo() }, Use('#next')),
+    ]),
   ])
 
 app({
@@ -52,17 +59,16 @@ app({
     track: {},
     iOS: /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream,
   },
-  events: {
-    update: console.log
-  },
   actions: {
+    setId: (s,a,d) => ({ id: d }),
     nextVideo: (s,a,d) =>
       fetch(`${url}/video/${s.id}/next`)
-      .then(r => r.json()).then(a.setTrack).then(a.getVideo)
+      .then(r => r.json()).then(a.setTrack)
       .catch(console.log),
     getVideo: (s,a,d) =>
       fetch(`${url}/video/${s.id}`)
-      .then(r => r.json()).then(a.setTrack)
+      .then(r => r.json())
+      .then(a.setTrack)
       .catch(console.log),
     setTrack: (s,a,d) => ({ id: d.id || s.id, track: d }),
     decodeURL: (s,a,d) => {
@@ -72,12 +78,12 @@ app({
     }
   },
   events: {
+    action: (s,a,d) => console.log(s,d),
     route: (s,a,d) => {
       if (d.match === '/play/:id') {
-        s.id = d.params.id
+        a.setId(d.params.id)
         a.getVideo()
       }
-      return s
     }
   },
   view: [
--- src/player.js
+++ src/player.js
@@ -1,24 +1,25 @@
 export const Player = () => ({
   state: {
-    player: document.querySelector('audio'),
+    player: null,
+    playing: false,
+  },
+  events: {
+    loaded: (s,a,d) => ({ playing: s.iOS ? false : true })
   },
   actions: {
     playPause: (s,a,d) => {
-      s.player = document.querySelector('audio')
-      s.player.paused ? s.player.play() : s.player.pause()
+      s.player && s.player.paused ? s.player.play() : s.player.pause()
+      return { playing: !s.player.paused }
     },
-    backwards: (s,a,d) => {
-      s.player = document.querySelector('audio')
-      s.player.currentTime = Math.max(s.player.currentTime - 10, 0)
+    rewind: (s,a,d) => {
+      s.player && (s.player.currentTime = Math.max(s.player.currentTime - 10, 0))
     },
     forwards: (s,a,d) => {
-      s.player = document.querySelector('audio')
-      s.player.currentTime = Math.min(s.player.currentTime + 10, s.player.duration)
+      s.player && (s.player.currentTime = Math.min(s.player.currentTime + 10, s.player.duration))
     },
     iosDuration: (s,a,d) => {
-      s.player = document.querySelector('audio')
-      return s.player ? (s.player.currentTime > s.player.duration / 2) : false
-    }
+      s.player ? (s.player.currentTime > s.player.duration / 2) : false
+    },
   },
 })
 
