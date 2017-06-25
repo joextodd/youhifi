053ab079ba58f5dca46deb0ff0b5d3b4d828156c
Luke Jackson <lukejacksonn@gmail.com>
2017-06-25 22:55:19 +0100

    Fixes blue underline other search results styles

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import { input } from '../components/input'
 
 export default (s,a) =>
   h('page', {}, [
-    h('form', {}, input(s,a)),
+    h('header', {}, input(s,a)),
     h('search-', {}, s.search.map((item) =>
       h('a', { href: `/play/${item.id.videoId}` }, [
         h('img', { src: `https://img.youtube.com/vi/${item.id.videoId}/hqdefault.jpg` }),
