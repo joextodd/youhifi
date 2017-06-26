39d7ad7655229a8eb809b623ba026094306ca339
Joe Todd <joe.todd@bravebison.io>
2017-06-26 11:01:29 +0100

    Added infinite search scroll

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,15 @@ import { input } from '../components/input'
 export default (s,a) =>
   h('page', {}, [
     h('header', {}, input(s,a)),
-    h('search-', {}, s.search.map((item) =>
+    h('search-', {
+      oncreate: e => {
+        window.onscroll = () => {
+          document.body.scrollTop > 0 &&
+            (window.innerHeight + window.scrollY) >= document.body.scrollHeight &&
+              a.searchNext()
+        }
+      }
+    }, s.search.map((item) =>
       h('a', { href: `/play/${item.id.videoId}` }, [
         h('img', { src: `https://img.youtube.com/vi/${item.id.videoId}/hqdefault.jpg` }),
         h('title-', {}, item.snippet.title),
