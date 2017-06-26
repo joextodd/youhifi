7bec33fb8002864b7a21475f49efb58b2a330810
Luke Jackson <lukejacksonn@gmail.com>
2017-06-26 18:27:11 +0100

    Abstract out infinite scrolling list to huy

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,24 +1,24 @@
 import { h } from 'hyperapp'
-import { link, img, input } from 'huy'
+import { link, img, input, ul } from 'huy'
+
+const ytThumb = id =>
+  `https://img.youtube.com/vi/${id}/hqdefault.jpg`
 
 export default (s,a) =>
   h('page', {}, [
     h('header', {}, input({
       placeholder: 'Search Youtube',
+      value: s.searchString,
       action: a.search,
       debounce: 300,
     })),
-    h('search-', {
-      oncreate: e => {
-        window.onscroll = () => {
-          document.body.scrollTop > 0 &&
-            (window.innerHeight + window.scrollY) >= document.body.scrollHeight &&
-              a.searchNext()
-        }
-      }
-    }, s.search.map(item =>
+    ul({
+      class: 'search-results',
+      infinite: a.searchNext,
+    },
+    s.search.map(item =>
       link(a)({ href: `/play/${item.id.videoId}` },[
-        img({ src: `https://img.youtube.com/vi/${item.id.videoId}/hqdefault.jpg` }),
+        img({ src: ytThumb(item.id.videoId) }),
         h('title-', {}, item.snippet.title),
       ])
     ))
