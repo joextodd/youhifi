94090ff59afde67defe2a40bb6b127a460a2aa16
Joe Todd <joe.todd@bravebison.io>
2017-06-26 13:04:33 +0100

    Get next videos client side

--- src/components/input.js
+++ src/components/input.js
@@ -1,19 +1,19 @@
 import { h } from 'hyperapp'
 
 function debounce(func, wait, immediate) {
-  var timeout;
+  var timeout
   return function() {
     var context = this, args = arguments
     var later = function() {
-      timeout = null;
+      timeout = null
       if (!immediate) func.apply(context, args)
     }
     var callNow = immediate && !timeout
     clearTimeout(timeout)
     timeout = setTimeout(later, wait)
     if (callNow) func.apply(context, args)
-  };
-};
+  }
+}
 
 const debounced = fn => debounce(fn, 300)
 
