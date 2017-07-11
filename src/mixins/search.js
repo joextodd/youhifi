405af06b94d957a372142dbd1af2dda421d47a9a
Luke Jackson <lukejacksonn@gmail.com>
2017-07-11 23:21:24 +0100

    Extracts YT api calls to const functions

--- src/mixins/search.js
+++ src/mixins/search.js
@@ -1,75 +1,80 @@
+const MAX_RESULTS = 10
+const YT_API = "https://www.googleapis.com/youtube/v3"
+const YT_API_KEY = "AIzaSyBKrJKFGTSo3YVPyavzo6ngIll3DYIuadQ"
+
+const fetchRelated = id =>
+  fetch(`${YT_API}/search` +
+    '?part=snippet' +
+    `&maxResults=${MAX_RESULTS}` +
+    '&videoCategoryId=10' +
+    '&type=video' +
+    `&relatedToVideoId=${id}` +
+    `&key=${YT_API_KEY}`)
+  .then(r => r.json())
+
+const fetchSearch = query =>
+  fetch(`${YT_API}/search` +
+    '?part=snippet' +
+    `&maxResults=${MAX_RESULTS}` +
+    '&type=video' +
+    '&videoCategoryId=10' +
+    `&q=${query}` +
+    `&key=${YT_API_KEY}`)
+  .then(r => r.json())
+
+const fetchSearchNext = (query, token) =>
+  fetch(`${YT_API}/search` +
+    '?part=snippet' +
+    `&maxResults=${MAX_RESULTS}` +
+    '&type=video' +
+    '&videoCategoryId=10' +
+    `&q=${query}` +
+    `&pageToken=${token}` +
+    `&key=${YT_API_KEY}`)
+  .then(r => r.json())
+
 export const Search = () => ({
   state: {
-    url: 'https://www.googleapis.com/youtube/v3',
-    key: 'AIzaSyBudPwcEKAS7KEyMnyDOPuHUv5pd3vSZ-U',
-    results: 5,
     searchString: '',
     searchToken: '',
     search: [],
-    next: [],
   },
   events: {
     loaded: (s,a) => a.search('')
   },
   actions: {
     setSearchString: (s,a,d) => ({ searchString: d }),
-    setSearchToken: (s,a,d) => ({ searchToken: d.nextPageToken }),
-    setSearchNext: (s,a,d) => ({ search: s.search.concat(d.items) }),
-    setSearch: (s,a,d) => ({ search: d.items }),
-    setNext: (s,a,d) => ({ next: d.items }),
+    setSearchToken: (s,a,d) => ({ searchToken: d }),
+    setSearchNext: (s,a,d) => ({ search: s.search.concat(d) }),
+    setSearch: (s,a,d) => ({ search: d }),
     nextVideo: (s,a,d) => {
       a.pause()
       if (!s.partyId || (s.partyId && s.partyQ.length <= 1)) {
-        const idx = parseInt(Math.random() * (s.results - 1))
-        fetch(`${s.url}/search` +
-              '?part=snippet' +
-              `&maxResults=${s.results}` +
-              `&relatedToVideoId=${s.id}` +
-              '&videoCategoryId=10' +
-              '&type=video' +
-              `&key=${s.key}`)
-        .then(r => r.json())
-        .then(d => {
-          a.setNext(d)
-          s.partyId ?
-            a.savePartyState(d.items[idx].id.videoId) :
-              a.router.go(`/${d.items[idx].id.videoId}`)
+        const idx = parseInt(Math.random() * (MAX_RESULTS - 1))
+        fetchRelated(s.id).then(d => {
+          s.partyId
+            ? a.savePartyState(d.items[idx].id.videoId)
+            : a.router.go(`/${d.items[idx].id.videoId}`)
         })
         .catch(console.log)
       }
       s.partyId && a.nextQTrack()
     },
     search: (s,a,d) => {
-      a.setSearchString(d.target ? d.target.value : d)
-      fetch(`${s.url}/search` +
-            '?part=snippet' +
-            `&maxResults=${s.results}` +
-            `&q=${d.target ? d.target.value : d}` +
-            '&type=video' +
-            '&videoCategoryId=10' +
-            `&key=${s.key}`)
-      .then(r => r.json())
-      .then(d => {
-        a.setSearch(d)
-        a.setSearchToken(d)
-      })
-      .catch(console.log)
+      const query = d.target ? d.target.value : d
+      a.setSearchString(query)
+      fetchSearch(query)
+      .then(results => {
+        a.setSearch(results.items)
+        a.setSearchToken(results.nextPageToken)
+      }).catch(console.log)
     },
     searchNext: (s,a,d) => {
-      fetch(`${s.url}/search` +
-            '?part=snippet' +
-            `&maxResults=${s.results}` +
-            `&q=${s.searchString}` +
-            '&type=video' +
-            `&pageToken=${s.searchToken}` +
-            '&videoCategoryId=10' +
-            `&key=${s.key}`)
-      .then(r => r.json())
-      .then(d => {
-        a.setSearchNext(d)
-        a.setSearchToken(d)
-      })
-      .catch(console.log)
+      fetchSearchNext(s.searchString, s.searchToken)
+      .then(results => {
+        a.setSearchNext(results.items)
+        a.setSearchToken(results.nextPageToken)
+      }).catch(console.log)
     },
   },
 })
