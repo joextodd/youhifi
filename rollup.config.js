0a1ae1c0e76682314277b7f13d73557b40751e77
Joe Todd <joextodd@gmail.com>
2017-09-30 19:56:58 +0100

    Add firebase hosting

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,4 @@
-import buble from 'rollup-plugin-buble'
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import uglify from 'rollup-plugin-uglify'
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
-import postcss from 'rollup-plugin-postcss'
-import nested from 'postcss-nested'
-import autoprefix from "autoprefixer"
+import preroll from 'preroll'
 
 const prod = !process.env.ROLLUP_WATCH
 const dev = !!process.env.ROLLUP_WATCH
@@ -18,28 +10,8 @@ export default {
     sourcemap: dev ? 'inline' : false,
     format: 'iife',
   },
-  plugins: [
-    postcss({ plugins: [
-      nested(),
-      autoprefix({ browsers: 'last 2 versions' }),
-    ]}),
-    resolve({ jsnext: true }),
-    commonjs(),
-    buble({ jsx: 'h' }),
-    prod && uglify(),
-    dev && livereload('static'),
-    dev && serve({
-      contentBase: ['static'],
-      historyApiFallback: true,
-      port: 8080,
-    })
-  ],
+  plugins: [ ...preroll(dev) ],
   moduleContext: {
     'node_modules/whatwg-fetch/fetch.js': 'window',
-  },
-  onwarn: function (message) {
-    if (/Use of `eval` \(in .*\/node_modules\/firebase\/.*\) is strongly discouraged/.test(message)) {
-      return
-    }
   },
 }
