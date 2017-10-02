d2bd546826e9bedeeb93f1b3ade06fc0109cfb19
Luke Jackson <lukejacksonn@gmail.com>
2017-10-02 20:07:15 +0100

    Updates preroll and fixes css for modules (#24)

--- src/components/popup.js
+++ src/components/popup.js
@@ -5,9 +5,11 @@ import { $icon } from '../helpers/element'
 import Actuate from 'actuatejs'
 import animate from 'animate.css'
 
+import style from '../index.css'
+
 export default (a,d) =>
   div({
-    class: 'popup',
+    class: style.popup,
     oncreate: e => {
       Actuate(d.animations)(e).then(_ => a.setPopupVisible(false))
     }
--- src/helpers/window.js
+++ src/helpers/window.js
@@ -1,3 +1,5 @@
+import style from '../index.css'
+
 export const iOS = () => /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
 export const iOS_chrome = () => navigator.userAgent.match('CriOS')
 export const scrollToSearch = () =>
@@ -10,8 +12,8 @@ export const scrollToSearch = () =>
 export const focusOnScrollTop = {
   oncreate: e => {
     e._fn = ev => window.scrollY === 0
-      ? e.classList.add('focus')
-      : e.classList.remove('focus')
+      ? e.classList.add(style.focus)
+      : e.classList.remove(style.focus)
     e._fn()
     window.addEventListener('scroll', e._fn)
   },
